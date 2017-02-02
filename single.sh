97dd891debebd116b8e093cb0f13e1e96d3d574a
Patrick D'appollonio <dappollonio@hpe.com>
2017-02-02 11:09:40 -0700

    Add bash script to run tmux.
    Signed-off-by: Patrick D'appollonio <dappollonio@hpe.com>

---
+++ single.sh
@@ -0,0 +1,6 @@
+# Find if it's linux what we are running
+export VERSION=$(lsb_release -sc)
+export CODENAME=$(lsb_release -sr)
+
+# Run the actual command
+which tmux >/dev/null 2>&1 && { tmux attach || tmux new -s $VERSION-${CODENAME//./};  } || bash -l
