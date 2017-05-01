aa176a903044d04372bf19daef272ecd38c9befe
Patrick D'appollonio <dappollonio@hpe.com>
2017-05-01 13:41:28 -0600

    Update to remove buggy code.
    Signed-off-by: Patrick D'appollonio <dappollonio@hpe.com>

--- single.sh
+++ single.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -euo pipefail
 
 # Find if it's linux what we are running
 export VERSION=$(lsb_release -sc)
 export CODENAME=$(lsb_release -sr)
 
 # Run the actual command
-which tmux >/dev/null 2>&1 && { tmux attach || tmux new -s $VERSION-${CODENAME//./};  } || bash -l
+which tmux >/dev/null 2>&1 && { tmux attach || tmux new -s $VERSION-${CODENAME//./};  }
