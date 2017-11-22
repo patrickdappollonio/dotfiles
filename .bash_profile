0d827cf07960d5417c864c8b7664f7909c45005d
Patrick D'appollonio <patrick.dappollonio@dreamworks.com>
2017-11-21 22:35:43 -0800

    Update single script and bash_profile.

--- .bash_profile
+++ .bash_profile
@@ -134,7 +134,7 @@ fi
 
 # Enable NodeJS' NVM if path exists
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # Enable yarn package
--- single.sh
+++ single.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 # Find if it's linux what we are running
-version=""
-codename=""
-if ! [ -x "$(command -v lsb_release)" ]; then
-    version=$(cat /etc/system-release-cpe | awk -F: '{ print $3 }')
-    codename=$(cat /etc/system-release-cpe | awk -F: '{ print $5 }' | grep -o ^[0-9]*)
+if [ "$(uname)" == "Darwin" ]; then
+    IS_MAC_OS=true
+    IS_LINUX_OS=false
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    IS_LINUX_OS=true
 else
-    version=$(lsb_release -sc)
-    codename=$(lsb_release -sr)
+    IS_LINUX_OS=false
 fi
 
-export VERSION=$version
-export CODENAME=$codename
+# Env vars
+if [ "$IS_MAC_OS" = true ]; then
+    export VERSION="MacOS"
+    export CODENAME="Darwin"
+elif [ "$IS_LINUX_OS" = true ]; then
+    if ! [ -x "$(command -v lsb_release)" ]; then
+        export VERSION=$(cat /etc/system-release-cpe | awk -F: '{ print $3 }')
+        export CODENAME=$(cat /etc/system-release-cpe | awk -F: '{ print $5 }' | grep -o ^[0-9]*)
+    else
+        export VERSION=$(lsb_release -sc)
+        export CODENAME=$(lsb_release -sr)
+    fi
+fi
 
 # Run the actual command
 which tmux >/dev/null 2>&1 && { tmux attach || tmux new -s $VERSION-${CODENAME//./};  }
